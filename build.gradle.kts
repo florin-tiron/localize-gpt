d5cb94e7dec8fbd27fe0c3c0af7e1baa0506ff40
Florin Tiron <tironmihailflorin@gmail.com>
2023-11-01 00:04:13 +0200

    add READ.me

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,9 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-        implementation("com.squareup.okhttp3:okhttp:4.9.0")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
 }
 
 tasks.test {
